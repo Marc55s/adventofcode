598310c42f926b645d041e6e2b4cc817c12f0be7
marc55s <marc.schoenig@gmail.com>
2024-12-01 14:06:07 +0100

    fix day one

--- Python/2024/01.py
+++ Python/2024/01.py
@@ -2,29 +2,23 @@ from lib import *
 from collections import Counter
 
 input = read_input(2024, 1).strip()
-
 input = input.split("\n")
 
-
-a = []
-b = []
+left = []
+right = []
 
 for i in input:
     split = i.split("   ")
-    left = int(split[0])
-    right = int(split[1])
-    a.append(left)
-    b.append(right)
+    left_id = int(split[0])
+    right_id = int(split[1])
+    left.append(left_id)
+    right.append(right_id)
 
 
-z = a.copy()
-w = b.copy()
-z.sort()
-w.sort()
-distances = [abs(z[x]-w[x]) for x in range(len(a))]
+distances = [abs(left[x]-right[x]) for x in range(len(left))]
 
-similiarity = Counter(b)
-score = [int(x) * (similiarity[x]) for x in a]
+similiarity = Counter(right)
+score = [int(x) * (similiarity[x]) for x in left]
 
 print(sum(distances))
 print(sum(score))
