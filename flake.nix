f7aedc13898e16b694e45689c5f363b8a4aa0cf6
marc55s <marc.schoenig@gmail.com>
2024-12-03 23:19:59 +0100

    nix flake.lock added

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Advent of Code";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, treefmt-nix, rust-overlay }:
+    let
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit overlays;
+        system = "x86_64-linux";
+      };
+      rustVersion = pkgs.rust-bin.stable.latest.default;
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = rustVersion;
+        rustc = rustVersion;
+      };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs;[
+          rustc
+          cargo
+          python311
+        ];
+      };
+      formatter.x86_64-linux = treefmt-nix.lib.mkWrapper
+        nixpkgs.legacyPackages.x86_64-linux
+        {
+          projectRootFile = "flake.nix";
+          programs.nixpkgs-fmt.enable = true;
+          programs.rustfmt.enable = true;
+        };
+    };
+}
