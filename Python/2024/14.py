0f20766b5156f6cfabdbc15949273a9702bcceb4
Marc <marc.schoenig@gmail.com>
2024-12-15 16:27:31 +0100

    day 14

--- Python/2024/14.py
+++ Python/2024/14.py
@@ -1,5 +1,4 @@
 from time import sleep
-
 from lib import *
 
 w = 101
@@ -13,84 +12,6 @@ sec = 0
 third = 0
 fourth = 0
 
-
-def in_quadrant(x, y, vx, vy, seconds):
-    global first, sec, third, fourth
-    robots = []
-    for i in range(seconds):
-        x += vx
-        y += vy
-        if x >= w:
-            x %= w
-        elif x < 0:
-            x = w + x
-
-        if y >= h:
-            y %= h
-        elif y < 0:
-            y = h + y
-        robots.append((x, y))
-    if x == w // 2 or y == h // 2:
-        return robots
-
-    if x > w // 2:
-        if y > h // 2:
-            fourth += 1
-        else:
-            sec += 1
-    else:
-        if y > h // 2:
-            third += 1
-        else:
-            first += 1
-    return robots
-
-
-def display(robots):
-    for i in range(h):
-        for k in range(w):
-            if (k, i) in robots:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print()
-
-
-def build_rows(robots_positions):
-    res = [["." for x in range(w)] for z in range(h)]
-    for pos in robots_positions:
-        x, y = pos
-        res[y][x] = "#"
-    return res
-
-
-for second in range(10000):
-    if second % 100 == 0:
-        print(second)
-    r = []
-    for i in puzzle_input:
-        p, v = i.split(" ")
-        p = p.split(",")
-        px = p[0][2:]
-        py = p[1]
-        v = v.split(",")
-        vx = v[0][2:]
-        vy = v[1]
-        px = int(px)
-        py = int(py)
-        vx = int(vx)
-        vy = int(vy)
-        result = in_quadrant(px, py, vx, vy, second)
-        for z in result:
-            r.append(z)
-    rows = build_rows(r)
-    for row in rows:
-        if "#######" in row:
-            print(second)
-            break
-
-print(first, sec, third, fourth)
-print(first * sec * third * fourth)
 bots = []
 for i in puzzle_input:
     p, v = i.split(" ")
@@ -105,6 +26,43 @@ for i in puzzle_input:
     vx = int(vx)
     vy = int(vy)
     bots.append((px, py, vx, vy))
+
+def in_quadrant(x, y):
+    global first, sec, third, fourth
+    if x == w // 2 or y == h // 2:
+        return
+    if x > w // 2:
+        if y > h // 2:
+            fourth += 1
+        else:
+            sec += 1
+    else:
+        if y > h // 2:
+            third += 1
+        else:
+            first += 1
+
+def display(robots):
+    temp = []
+    for z in robots:
+        a,b,c,d = z
+        temp.append((a,b))
+    for i in range(h):
+        for k in range(w):
+            if (k, i) in temp:
+                print("#", end="")
+            else:
+                print(".", end="")
+        print()
+
+
+def build_rows(robots_positions):
+    res = [["." for x in range(w)] for z in range(h)]
+    for pos in robots_positions:
+        x,y,vx,vy = pos
+        res[y][x] = "#"
+    return res
+
 
 def update_bots(bots):
     updated = []
@@ -123,3 +81,26 @@ def update_bots(bots):
         updated.append((x,y,vx,vy))
     return updated
 
+def check_easter_egg(bots):
+    rows =  build_rows(bots)
+    for row in rows:
+        if "########" in "".join(row):
+            return True
+    return False
+
+
+
+for seconds in range(100000):
+    if seconds % 100 == 0:
+        print(seconds)
+    bots = update_bots(bots)
+    if check_easter_egg(bots):
+        display(bots)
+        print("Easter egg found at:",seconds+1) # seconds start at 1
+        break
+
+
+for bot in bots:
+    x,y,vx,vy = bot
+    in_quadrant(x,y)
+print(first*sec*third*fourth)
