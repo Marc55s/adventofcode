031217537adbc238f77950cd0809b487408753c2
marc55s <marc.schoenig@gmail.com>
2024-12-21 20:40:01 +0100

    day 19 part 1

--- Python/2024/19.py
+++ Python/2024/19.py
@@ -1,15 +1,38 @@
-from Python.lib.lib import read_input
+from lib import *
 
 puzzle_input = read_input(2024, 19).strip()
 puzzle_input = puzzle_input.split("\n")
 
 split_idx = puzzle_input.index("")
 patterns = puzzle_input[0:split_idx]
+patterns = patterns[0].split(", ")
 designs = puzzle_input[split_idx+1:]
 
 
-def dp(design, idx):
+dic = {}
+found = False
 
-    if design[0:idx] in patterns:
-        design = design[idx+1:]
 
+def dp(design, old):
+    global found
+    if found:
+        return
+    for i in patterns:
+        cutted = design.removeprefix(i)
+        if cutted == "":
+            found = True
+            print(old)
+            if old not in dic:
+                dic[old] = 1
+            else:
+                dic[old] += 1
+            return True
+        if cutted != design:
+            dp(cutted, old)
+
+
+ans = 0
+for d in designs:
+    found = False
+    dp(d, d)
+print(len(dic))
