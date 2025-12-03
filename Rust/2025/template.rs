97e3485f6b5a212aa440ef62da4ebcc577973c12
marc55s <marc.schoenig@gmail.com>
2025-12-03 21:30:25 +0100

    feat: add daily template

---
+++ Rust/2025/template.rs
@@ -0,0 +1,11 @@
+pub fn setup(input: &str) -> Vec<&str> {
+    input.lines().map(|s| s.to_string()).collect()
+}
+pub fn part1(input: &str) -> Vec<&str> {
+
+}
+pub fn part2(input: &str) -> Vec<&str> {
+
+}
+
+aoc::main!(2025, 3, part1, part2);
