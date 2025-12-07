bb4f504eaa69d2be7e2aa4632ecc76500c1c4f4d
marc55s <marc.schoenig@gmail.com>
2025-12-07 04:30:05 +0100

    update: template.rs

--- Rust/2025/template.rs
+++ Rust/2025/template.rs
@@ -1,11 +1,11 @@
 pub fn setup(input: &str) -> Vec<&str> {
     input.lines().map(|s| s.to_string()).collect()
 }
-pub fn part1(input: &str) -> Vec<&str> {
-
+pub fn part1(input: &Vec<&str>) -> i32 {
+    0
 }
-pub fn part2(input: &str) -> Vec<&str> {
-
+pub fn part2(input: &Vec<&str>) -> i32 {
+    0
 }
 
-aoc::main!(2025, 3, part1, part2);
+aoc::main!(2025, 0, part1, part2); // update input day
