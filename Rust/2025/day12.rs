51b0e6cdf223f5fcc623fcaf47ddecfc499c75cd
marc55s <marc.schoenig@gmail.com>
2025-12-13 03:45:44 +0100

    solution: day12

---
+++ Rust/2025/day12.rs
@@ -0,0 +1,35 @@
+pub fn setup(input: &str) -> Vec<String> {
+    input.lines().map(|s| s.to_string()).collect()
+}
+pub fn part1(input: &Vec<String>) -> i32 {
+    let mut regions: Vec<_> = input.iter().filter(|s| s.contains('x')).collect();
+    let is_fitting: i32 = regions
+        .iter()
+        .map(|s| {
+            if let Some((area, patterns)) = s.split_once(": "){
+                let area: i32 = area.split("x").map(|s| s.parse::<i32>().unwrap()).product();
+
+                let all_patterns = patterns.split_whitespace()
+                    .map(|s| s.parse::<i32>().unwrap())
+                    .collect::<Vec<i32>>()
+                    .iter()
+                    .sum::<i32>();
+                
+                if area >= 9 * all_patterns {
+                    1
+                } else {
+                    0
+                }
+            } else {
+                0
+            }
+        })
+        .sum();
+    
+    is_fitting
+}
+pub fn part2(input: &Vec<String>) -> i32 {
+    0
+}
+
+aoc::main!(2025, 12, part1, part2); // update input day
